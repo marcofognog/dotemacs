8f8312f829c44b1831043e8d776a58f8d83a6f79
Marcao <marcofognog@gmail.com>
2017-07-29 09:15:46 -0300

    add inf ruby.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (fill-column-indicator rubocop rspec-mode magit helm-projectile heap goto-last-change flycheck ack))))
+    (inf-ruby fill-column-indicator rubocop rspec-mode magit helm-projectile heap goto-last-change flycheck ack))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
