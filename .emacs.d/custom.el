64761b235ac1157c81daba2bc4758e8c4603543e
Marcao <marcofognog@gmail.com>
2017-08-01 17:52:30 -0300

    No blinking for cursor.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
  '(package-selected-packages
    (quote
     (inf-ruby fill-column-indicator rubocop rspec-mode magit helm-projectile heap goto-last-change flycheck ack))))
