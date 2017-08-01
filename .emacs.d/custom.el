53788540d3e7124beea6c82cf9f223635e88b648
Marcao <marcofognog@gmail.com>
2017-08-01 19:26:52 -0300

    auto dim other buffers.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,13 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-dim-other-buffers-mode t)
  '(blink-cursor-mode nil)
  '(package-selected-packages
    (quote
-    (inf-ruby fill-column-indicator rubocop rspec-mode magit helm-projectile heap goto-last-change flycheck ack))))
+    (auto-dim-other-buffers inf-ruby fill-column-indicator rubocop rspec-mode magit helm-projectile heap goto-last-change flycheck ack))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-dim-other-buffers-face ((t (:background "#133038"))))
  '(cursor ((t (:background "dark violet")))))
