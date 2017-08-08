2f727b8d3aa9fb80541beb6004017bfe9bfb510c
Marcao <marcofognog@gmail.com>
2017-08-08 13:51:23 -0300

    Change face to dejaVu

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -13,5 +13,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
  '(auto-dim-other-buffers-face ((t (:background "#133038"))))
  '(cursor ((t (:background "dark violet")))))
