74924581dd151d5338da223322fd35f44db1f4a9
Marcao <marcofognog@gmail.com>
2017-09-13 13:37:46 -0300

    add function for deleting files.

--- .emacs.d/conf/init-custom.el
+++ .emacs.d/conf/init-custom.el
@@ -22,3 +22,15 @@
  (progn 	(copy-file filename newname 1) 	(delete-file filename) 	(set-visited-file-name newname) 	(set-buffer-modified-p nil) 	t))))
 
  (provide 'init-custom)
+
+(defun delete-file-and-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (when filename
+      (if (vc-backend filename)
+          (vc-delete-file filename)
+        (progn
+          (delete-file filename)
+          (message "Deleted file %s" filename)
+          (kill-buffer))))))
