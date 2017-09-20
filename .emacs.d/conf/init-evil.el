20e3004ed0a13880c8df538854de4fbe9dc12bd0
Marcao <marcofognog@gmail.com>
2017-09-20 10:00:04 -0300

    Unbind evil mode key in favor of ctrlnum.

--- .emacs.d/conf/init-evil.el
+++ .emacs.d/conf/init-evil.el
@@ -1,5 +1,9 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/evil")
 (require 'evil)
 
+;; Free up this key to use in ctrlnum
+(define-key evil-motion-state-map (kbd "C-6") nil)
+
+
 (evil-mode 1)
 (provide 'init-evil)
