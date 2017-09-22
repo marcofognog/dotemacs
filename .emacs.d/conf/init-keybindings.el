486dca3fcec1fb238fe1d1abc4a598ae9e7c46ef
Marcao <marcofognog@gmail.com>
2017-09-22 09:54:46 -0300

    add key bindings.

--- .emacs.d/conf/init-keybindings.el
+++ .emacs.d/conf/init-keybindings.el
@@ -7,12 +7,13 @@
 (global-set-key (kbd "s-V") 'helm-show-kill-ring)
 
 (global-set-key (kbd "C-c f") 'magit-status)
-(global-set-key (kbd "C-c d") 'magit-checkout)
-(global-set-key (kbd "C-c s") 'magit-log-head)
 
 (global-set-key (kbd "C-c p s a") 'helm-projectile-ack)
 
 (global-set-key (kbd "M-u") 'previous-buffer)
 (global-set-key (kbd "M-i") 'next-buffer)
+
+(global-set-key (kbd "C-c q") 'eshell)
+(global-set-key (kbd "C-c w") 'whitespace-cleanup)
 
 (provide 'init-keybindings)
