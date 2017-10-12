d3301c659419029c236e1039b53458a41f1a4577
Marcao <marcofognog@gmail.com>
2017-10-12 11:32:15 -0300

    This is awkward

--- .emacs.d/conf/init-ui.el
+++ .emacs.d/conf/init-ui.el
@@ -1,31 +1,3 @@
-;;; init-ui.el --- All settings for UI
-;;
-;; Copyright (C) 2015 Brunno dos Santos <emacs at brunno dot me>
-;;
-;; Author: Brunno dos Santos @squiter
-;; URL: http://github.com/squiter/emacs-fast-start
-;;
-;; This file is NOT part of GNU Emacs.
-;;
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-;;
-;; See <http://www.gnu.org/licenses/> for a copy of the GNU General
-;; Public License.
-;;
-;;; Commentary:
-;;
-;; Everything related to visual is here.
-
-;;; Code:
-
 (setq
  ;; better startup
  inhibit-splash-screen t
