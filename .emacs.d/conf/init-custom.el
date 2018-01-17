c65c57e7c953b319863b21605ac05558aa0a1e0a
Marcao <marcofognog@gmail.com>
2018-01-17 11:22:23 -0200

    do not use version control when deleting a file

--- .emacs.d/conf/init-custom.el
+++ .emacs.d/conf/init-custom.el
@@ -28,9 +28,7 @@
   (interactive)
   (let ((filename (buffer-file-name)))
     (when filename
-      (if (vc-backend filename)
-          (vc-delete-file filename)
-        (progn
-          (delete-file filename)
-          (message "Deleted file %s" filename)
-          (kill-buffer))))))
+      (progn
+        (delete-file filename)
+        (message "Deleted file %s" filename)
+        (kill-buffer)))))
