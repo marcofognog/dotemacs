443daefc942a1f85f4a2be8527e329b3d444dfd5
Marcao <marcofognog@gmail.com>
2018-05-23 09:29:33 -0300

    Oops, this is not mine

--- .emacs.d/conf/init-mail.el
+++ .emacs.d/conf/init-mail.el
@@ -18,9 +18,6 @@
 
 (setq mail-user-agent 'message-user-agent)
 
-(setq user-mail-address "chrisdone@gmail.com"
-      user-full-name "Chris Done")
-
 (setq smtpmail-stream-type 'ssl
       smtpmail-smtp-server "smtp.gmail.com"
       smtpmail-smtp-service 465)
