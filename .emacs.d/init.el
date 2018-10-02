a162d272716e2efd709a6f0c53ab4592a9806c67
Marcao <marcofognog@gmail.com>
2018-10-02 11:11:34 -0300

    add comment and mix format hook

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -35,7 +35,13 @@
 (put 'upcase-region 'disabled nil)
 
 ;; Copy $PATH env from shell so elixir-mode can find mix in ~/./asdf folder
+;; TODO: maybe it is possible to config mix path directly to elixir-mode?
 (exec-path-from-shell-initialize)
+
+;; mix format before saving
+(add-hook 'elixir-mode-hook
+          (lambda () (add-hook 'before-save-hook 'elixir-format nil t)))
+
 ;; Redefining this function makes it work -> possible bug in projectile?
 (defun projectile-discover-projects-in-directory (directory)
   "Discover any projects in DIRECTORY and add them to the projectile cache.
