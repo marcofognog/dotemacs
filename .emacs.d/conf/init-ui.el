1ac55bb72ddfe8b1cb28d36218010b0351667aa3
Marcao <marco.nogueira@experteer.com>
2019-05-07 18:15:04 +0200

    Smaller fonts

--- .emacs.d/conf/init-ui.el
+++ .emacs.d/conf/init-ui.el
@@ -14,7 +14,7 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
-(set-face-attribute 'default nil :height 130)
+(set-face-attribute 'default nil :height 120)
 
 ;; line numbers
 (add-hook 'prog-mode-hook 'linum-mode)
