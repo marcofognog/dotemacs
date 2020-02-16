c7f6e8861b7349629cbab55ea7359d0c9a7cdf24
Marcao <marcofognog@gmail.com>
2020-02-16 17:49:21 -0300

    save excursion

--- .emacs.d/conf/init-toggle-truth.el
+++ .emacs.d/conf/init-toggle-truth.el
@@ -2,12 +2,14 @@
   "Replace boolean tokens by their oposite value"
   (interactive)
 
-  (goto-char (line-beginning-position))
-  (while (search-forward "true" (line-end-position) t)
-    (replace-match "false"))
+  (save-excursion
+    (goto-char (line-beginning-position))
+    (while (search-forward "true" (line-end-position) t)
+      (replace-match "false"))
 
-  (while (search-forward "false" (line-end-position) t)
-    (replace-match "true"))
+    (while (search-forward "false" (line-end-position) t)
+      (replace-match "true"))
+    )
   )
 
 (provide 'init-toggle-truth)
